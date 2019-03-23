815949a13e520d2a904795eb130e90a0d04e8fd8
nileshpardeshi <nileshpardeshi2507@gmail.com>
2019-03-23 16:05:33 +0530

    removed space

--- src/Hello.java
+++ src/Hello.java
@@ -5,8 +5,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("hello git");
 		System.out.println("second commit");
-		
-		//featre 1 branch changes
+		//feature 1 branch changes
 		System.out.println("I am feature 1");
 		
 		
