38cdcd4b054243b0487aee9a25e2da8722f93b9c
nileshpardeshi <nileshpardeshi2507@gmail.com>
2019-03-23 16:11:33 +0530

    resolved

--- src/Hello.java
+++ src/Hello.java
@@ -5,9 +5,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("hello git");
 		System.out.println("second commit");
-		//changes by feature 2 branch
-		System.out.println("feature 2 branch changes");
-		System.out.println("I am learining conflicts");
+	
 		
 		
 
