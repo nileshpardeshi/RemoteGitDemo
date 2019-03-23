70f1f74383a675f5925bf1296bc80f592a5f8035
nileshpardeshi <nileshpardeshi2507@gmail.com>
2019-03-23 15:48:59 +0530

    Feature 1 first commit

--- src/Hello.java
+++ src/Hello.java
@@ -6,7 +6,8 @@ public class Hello {
 		System.out.println("hello git");
 		System.out.println("second commit");
 		
-		System.out.println("old branch commit");
+		//featre 1 branch changes
+		System.out.println("I am feature 1");
 		
 		
 
